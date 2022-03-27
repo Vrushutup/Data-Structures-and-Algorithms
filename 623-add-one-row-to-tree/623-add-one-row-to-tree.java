5c1bdcb4f4b32f3f7c15f7c3b917dbe2b25b0154
Vrushabh V Shamkuwar <vrushabhs1998@gmail.com>
2022-03-27 10:38:37 +0530

    Time: 0 ms (100.00%), Space: 44.9 MB (70.88%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.java
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.java
@@ -16,42 +16,20 @@
 class Solution {
     public TreeNode addOneRow(TreeNode root, int val, int depth) {
         if(depth==1){
-            TreeNode a=new TreeNode(val);
-            a.left=root;
-            return a;
+            return new TreeNode(val,root,null);
+        }        
+         if(depth==2){    
+            TreeNode nl=new TreeNode(val,root.left,null);
+             root.left=nl;
+            TreeNode nr=new TreeNode(val,null,root.right);
+             root.right=nr;
+         }
+        if(root.left!=null) {
+            addOneRow(root.left,val,depth-1);
         }
-        Queue<TreeNode> q=new ArrayDeque();
-        q.add(root);
-        while(q.size()!=0){
-            int size=q.size();
-            while(size-->0){
-                TreeNode rnode=q.remove();
-                
-                if(depth==2){    
-                  TreeNode l=rnode.left;
-                  TreeNode x=new TreeNode(val);
-                  rnode.left=x;
-                  x.left=l;
-                    
-                  TreeNode r=rnode.right;
-                  TreeNode y=new TreeNode(val);
-                  rnode.right=y;
-                  y.right=r;
-                }
-                else{
-                if(rnode.left!=null){
-                    q.add(rnode.left);
-                }
-                if(rnode.right!=null){
-                    q.add(rnode.right);
-                } 
-                }
-            }
-                
-            if(depth==2)return root;
-            depth--;
-        }
-        
+       if(root.right!=null){  
+        addOneRow(root.right,val,depth-1);
+       }
         return root;
     }
 }
