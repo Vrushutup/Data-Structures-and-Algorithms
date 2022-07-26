15b6ff65b3ee5bc1377dd7f829392154c90ac46e
Vrushabh V Shamkuwar <vrushabhs1998@gmail.com>
2022-07-26 09:15:47 +0530

    Time: 6 ms (43.67%), Space: 55 MB (56.22%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -1,13 +1,6 @@
 class Solution {
     List<List<Integer>> Ans;
-    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        Ans=new ArrayList<>();
-     List<Integer> path=new ArrayList<>();
-     path.add(0);
-     allPath(path,graph,0);
-     return Ans;
-    }
-    private void allPath(List<Integer> path,int[][] graph, int i){
+    public void allPath(List<Integer> path,int[][] graph, int i){
         if(graph.length-1==i){
             Ans.add(path);
             return;
@@ -17,5 +10,12 @@ class Solution {
             newPath.add(node);
             allPath(newPath,graph,node);
         }
+    }
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        Ans=new ArrayList<>();
+     List<Integer> path=new ArrayList<>();
+     path.add(0);
+     allPath(path,graph,0);
+     return Ans;
     }
 }
